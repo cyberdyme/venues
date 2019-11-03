13aa0470de542ea3c838f43d55949d30eb479a69
Girish Thanki <girish.thanki@gmail.com>
2019-11-03 21:34:22 +0000

    get focus working

--- src/app/tranche/mandate.component.ts
+++ src/app/tranche/mandate.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { VenuesIssuenetService, JournalResponseContainer } from '../service/venues-issuenet.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-mandate',
@@ -7,12 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MandateComponent implements OnInit {
   public currentTab = 0;
-  constructor() { }
+  public venuesIssuenetService$: Observable<JournalResponseContainer> = null;
+
+  constructor(private venuesIssuenetService: VenuesIssuenetService) { }
 
   ngOnInit() {
   }
 
   setTabIndex($event) {
     this.currentTab = $event + 1;
+
+    console.log('CURRENT INDEX:', this.currentTab);
+    this.venuesIssuenetService$ = this.venuesIssuenetService.venueIssuenetStream$(this.currentTab);
+  }
+
+  onFocusChanged($event) {
   }
 }
